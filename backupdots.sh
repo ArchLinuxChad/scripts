24a19f42b984b03a065e7b85134d4c7e5fb942a2
ArchLinuxChad <thedillonking@proton.me>
2024-11-07 19:11:09 +0000

    updated

--- backupdots.sh
+++ backupdots.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+configrepo="/home/$USER/.config"
+dotsrepo="/home/$USER/github-repos/dotfiles"
+
 # backup files
-cp /home/$USER/.bashrc /home/$USER/github-repos/dotfiles/
+cp /home/$USER/.bashrc $dotsrepo
 
 # back up .config
-cp /home/$USER/.config/doom /home/$USER/github-repos/dotfiles/.config/
+cp -r $configrepo/dunst $dotsrepo/.config/
+cp -r $configrepo/fastfetch $dotsrepo/.config/
+cp -r $configrepo/kitty $dotsrepo/.config/
+cp -r $configrepo/picom $dotsrepo/.config/
+cp $configrepo/starship.toml $dotsrepo/.config/
