6e93a6543af8d90d9ce9d56f1f19d5b58dee3ab7
ArchLinuxChad <thedillonking@proton.me>
2024-11-09 20:29:21 +0000

    update

---
+++ setupscripts.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo cp bar.sh /usr/local/bin/bar
+sudo cp updatebar.sh /usr/local/bin/updatebar
+sudo cp incvol.sh /usr/local/bin/incvol
+sudo cp decvol.sh /usr/local/bin/decvol
+sudo cp dots.sh /usr/local/bin/dots
+sudo cp power.sh /usr/local/bin/power
